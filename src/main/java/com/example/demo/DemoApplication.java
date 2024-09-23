15d1cb72122492ce7f7baf993cc645d346de390d
Aaron Ryu <fbcndah@gmail.com>
2024-09-23 21:23:50 +0900

    Init: Spring Initializr 통한 Spring/Java 프로젝트 시작

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+    }
+}
