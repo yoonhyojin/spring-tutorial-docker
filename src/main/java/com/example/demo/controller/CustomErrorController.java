bc45af242d19f047737103b0ee21899c2c4efcc7
Aaron Ryu <fbcndah@gmail.com>
2024-10-01 14:29:49 +0900

    실습 5-7: CustomErrorController : ErrorController 인터페이스 커스텀 구현체 2) ModelAndView 반환

--- src/main/java/com/example/demo/controller/CustomErrorController.java
+++ src/main/java/com/example/demo/controller/CustomErrorController.java
@@ -7,21 +7,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping(value = "/error", method = RequestMethod.GET)
-    public String handleError(HttpServletRequest request) {
+    public ModelAndView handleError(HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView();
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error/404";
+                modelAndView.addObject("status", statusCode);
+                modelAndView.addObject("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
             } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error/500";
+                modelAndView.addObject("status", statusCode);
+                modelAndView.addObject("reason", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
             }
         }
-        return "error";
+        modelAndView.setViewName("error");
+        return modelAndView;
     }
 }
