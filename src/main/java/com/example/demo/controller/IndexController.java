fdacf3dbe6a6062cad2df563cce6c876e54e3cb6
Aaron Ryu <fbcndah@gmail.com>
2024-10-01 23:36:39 +0900

    실습 5-24: @RestController = 메서드 단위의 @ResponseBody + 클래스 단위의 @Controller

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
     public String index() {
         return "index";
 //      return "index.html";
