6abddfd9b0a9216df1eaf29899b1ceefd946f00c
Aaron Ryu <fbcndah@gmail.com>
2024-10-01 20:01:14 +0900

    실습 5-17: ModelAndView 사용 시 개발자 직접 생성이 아닌 Spring 으로부터 생성된 Bean 객체 사용

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -14,15 +14,10 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
     @Autowired
-    private List<UserServiceInterface> userServices;
+    private UserServiceInterface AUserService;
 
     @GetMapping("")
-    public ModelAndView userPage() {
-        ModelAndView modelAndView = new ModelAndView();
-        userServices.forEach((each) -> {
-            System.out.println(each.getClass().getSimpleName());
-        });
-        UserServiceInterface AUserService = userServices.get(0);
+    public ModelAndView userPage(ModelAndView modelAndView) {
         List<User> users = AUserService.findAll();
         modelAndView.addObject("users", users);
         modelAndView.setViewName("/users/list");
@@ -30,12 +25,7 @@ public class UserController {
     }
 
     @GetMapping("/1/detail")
-    public ModelAndView detailPage() {
-        ModelAndView modelAndView = new ModelAndView();
-        userServices.forEach((each) -> {
-            System.out.println(each.getClass().getSimpleName());
-        });
-        UserServiceInterface AUserService = userServices.get(0);
+    public ModelAndView detailPage(ModelAndView modelAndView) {
         User user = AUserService.findById(1);
         modelAndView.addObject("id", user.getId());
         modelAndView.addObject("name", user.getName());
