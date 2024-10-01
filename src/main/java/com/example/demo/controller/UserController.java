5784931e200cbd5efe3375a6de883c9f950a7761
Aaron Ryu <fbcndah@gmail.com>
2024-10-01 19:14:59 +0900

    실습 5-10: Thymeleaf : ModelAndView 통해 데이터(Model)을 .html(ViewTemplate)에 대입

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,16 +3,26 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
     @RequestMapping(method = RequestMethod.GET, value = "/users")
-    public String userPage() {
-        return "/users/list";
+    public ModelAndView userPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/users/list");
+        return modelAndView;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/users/1/detail")
-    public String detailPage() {
-        return "/users/detail";
+    public ModelAndView detailPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("id", 1);
+        modelAndView.addObject("name", "Aaron");
+        modelAndView.addObject("age", 10);
+        modelAndView.addObject("job", "Developer");
+        modelAndView.addObject("specialty", "Backend");
+        modelAndView.setViewName("/users/detail");
+        return modelAndView;
     }
 }
